901d8555d5f5a6931d7ca96fb22d4756e02a63e9
Tuğba Yılmaz <tugbayill01@gmail.com>
2025-11-29 18:55:18 +0300

    added EF Core migration for new user relationships

--- WishlistApi/Migrations/20251129113851_AddUserRelationships.Designer.cs
+++ WishlistApi/Migrations/20251129113851_AddUserRelationships.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WishlistApi.Data;
@@ -11,9 +12,11 @@ using WishlistApi.Data;
 namespace WishlistApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251129113851_AddUserRelationships")]
+    partial class AddUserRelationships
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
