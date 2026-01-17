f7b6dde492776176915f884b52173c3a9d4cbf9b
Tuğba Yılmaz <tugbayill01@gmail.com>
2026-01-17 14:25:27 +0300

    configured database connection for deployment and added Docker support

--- WishlistApi/Program.cs
+++ WishlistApi/Program.cs
@@ -10,9 +10,14 @@ Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 
-var baseConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
-var connectionString = $"{baseConnectionString}Password={dbPassword};";
+var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    var baseConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+    connectionString = $"{baseConnectionString}Password={dbPassword};";
+}
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
